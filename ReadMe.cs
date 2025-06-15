067acf37330e1fe0c96313a0b5dc0ba4d53145ad
Joel-wong-al <s10266141@connect.np.edu.sg>
2025-06-15 15:50:26 +0800

    ReadMe
    Made my ReadMe file

--- ReadMe.cs
+++ ReadMe.cs
@@ -1,16 +1,44 @@
-using UnityEngine;
+//ReadMe.cs
 
-public class ReadMe : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+// Key Controls
+// W = Move forward
+// A = Move Left
+// D = Move Right
+// S = Move Back
+// E = Interact (allow player to open door and collect items)
+// Space Bar = Jump
+// Shift = Run
+// Game Hack
+// N.A
+// Answer Key
+// Need 10 score to open the door
+// How to play the game
+// Elements:
+// Lava = Instant Death
+// Lazer = 5 damage
+// Escape Door = Need more than 10 Score to open
+// Potion = 1 Score each
+// Rocks and pipes = Safe
+// Normal Door = Open and close once player is out of range
+// Start:
+// Firstly the player will have to collect the first potion, then navigate around the rocks and pipes while trying not to drop into the lava, then they will find there is no way for them to escape through that, and will be told to look around, then the player should be able to spot the escape door under the starting platform and open it.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+// Lazer Room:
+// Player will have to collect 5 potions while avoiding the lazer so as to not take damage and die. If player dies to lazer he will respawn back to the start of the game.
+
+// Maze:
+// The player has to try and find 4 potions inside the maze while trying to find the exit for it to escape. After that the player will find a stairs up and just follow it to the exit
+
+// Exit:
+// Player will find the escape door and if player has 10 points he can escape or else he will have to jump into the lava to restart. Once player escape there will be a screen showing saying congrats player has escaped.
+
+// Desired platform and Settings
+// Platform:
+// Computers / Laptops
+// Settings:
+// Lazer Damage should be set to 5 so player will not die too fast
+// Health should be set to 100 
+// Score for each potion should be 1 score so that player will have to collect all the potion to escape
+
+// Limitations or Bugs
+// N.A
